e6ada0911bb853b2a335eb11051e9b07259d1b20
wjj <13524220689@189.cn>
2016-06-08 00:32:19 +0800

    fix bug

--- viewport.js
+++ viewport.js
@@ -11,6 +11,6 @@
 function viewport (width, fontSize) {
     var doc = document;
     var rootElement = doc.documentElement;
-    doc.getElementsByTagName('head')[0].appendChild('<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />');
+    document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />');
     rootElement.style.fontSize = rootElement.clientWidth / width * fontSize + 'px';              
 }
